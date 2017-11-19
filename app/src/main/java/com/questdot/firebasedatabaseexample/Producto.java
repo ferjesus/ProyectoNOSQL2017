2dd4acc9a8fc15613ebee173db164e9641624853
Fernando Rodriguez <ferjesus1710@gmail.com>
2017-11-19 18:56:26 -0500

    Fernando Rodriguez Lopez

---
+++ app/src/main/java/com/questdot/firebasedatabaseexample/Producto.java
@@ -0,0 +1,48 @@
+package com.questdot.firebasedatabaseexample;
+
+public class Producto {
+
+    String key;
+
+    String nombre;
+    int precio;
+    String link;
+
+    public Producto(String key, String nombre, int precio) {
+        this.key = key;
+        this.nombre = nombre;
+
+        this.precio = precio;
+    }
+    public Producto(String key, String nombre, int precio,String link) {
+        this.key = key;
+        this.nombre = nombre;
+
+        this.precio = precio;
+        this.link = link;
+
+    }
+
+    public Producto() {
+    }
+
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getName() {
+        return nombre;
+    }
+
+
+
+    public int getPrecio() {
+        return precio;
+    }
+    public String getLink() {
+        return link;
+    }
+
+}
